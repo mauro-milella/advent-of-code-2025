7b0a85ba7c93f2da218a83e7b3d0e2aaf788e4cf
mauro-milella <milella_mauro@live.it>
2025-12-01 07:50:18 +0100

    [ADD] dirty day 1 solution 1

---
+++ src/day1/solution1.lua
@@ -0,0 +1,51 @@
+-- Idea for refactoring the (dirty) solution:
+-- I could work with metatables and overload the _add behavior
+
+package.path = package.path .. ";../?.lua"
+
+local utils = require("utils")
+
+local input = {
+    directions = {},
+    values = {}
+}
+
+-- `parse_file` both reads the content of a file and parses it with a closure callback
+utils.parse_file("input.txt", function(s)
+    for line in s:gmatch("[^\n]+") do
+        local rot, val = line:match("(%L)(%d+)")
+        table.insert(input.directions, rot)
+        table.insert(input.values, tonumber(val))
+    end
+end)
+
+local lock_value = 50
+
+function rotate(val, direction, ticks)
+    if ticks >= 100 then
+        ticks = math.fmod(ticks, 100)
+    end
+
+    if direction == "R" then
+        return math.fmod(val + ticks, 100)
+    else
+        val = val - ticks
+        if val < 0 then
+            val = 100 + val
+        end
+
+        return val
+    end
+end
+
+local ans = 0
+
+for i = 1, #input.directions do
+    lock_value = rotate(lock_value, input.directions[i], input.values[i])
+
+    if lock_value == 0 then
+        ans = ans + 1
+    end
+end
+
+print(ans)
