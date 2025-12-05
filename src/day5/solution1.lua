ced9c6077feee8f2a808500d3ac544283f627de9
mauro-milella <milella_mauro@live.it>
2025-12-05 17:28:02 +0100

    [ADD] day 5 solution 1

--- src/day5/solution1.lua
+++ src/day5/solution1.lua
@@ -9,18 +9,27 @@ local ranges = {}
 
 ---Solve one query of the problem
 ---@param ranges table -- the list of fresh item ranges
----@param query table -- the item to look for
+---@param query integer -- the item to look for
 local function solve(ranges, query)
+    for i = 1, #ranges do
+        if query >= ranges[i][1] and query <= ranges[i][2] then
+            return 1
+        end
+    end
+    
 
+    return 0
 end
 
 utils.parse_file("input.txt", function(s)
     for l, r in s:gmatch("(%d+)%-(%d+)\n") do
-        table.insert(ranges, {l,r})
+        table.insert(ranges, {tonumber(l), tonumber(r)})
     end
 
-    for n in s:gmatch("%d\n") do
-        ans = ans + solve(ranges, n)
+    local second_section_idx = s:find("\n\n")
+
+    for query in s:sub(second_section_idx, #s):gmatch("%d+") do
+        ans = ans + solve(ranges, tonumber(query))
     end
 end)
 
